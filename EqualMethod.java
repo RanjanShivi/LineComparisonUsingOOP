2b9b9f63fbdc2643d1b0eff3eb73f87a97569f51
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-16 21:27:06 +0530

    Check if length of two lines are equal or not

---
+++ EqualMethod.java
@@ -0,0 +1,12 @@
+package com;
+
+class EqualMethod{
+
+    public boolean EqualMethod(Integer lengthOfLine1, Integer lengthOfLine2) {
+        System.out.println("Equals Method Result:");
+        if(lengthOfLine1.equals(lengthOfLine2))
+            return true;
+        else
+            return false;
+    }
+}
