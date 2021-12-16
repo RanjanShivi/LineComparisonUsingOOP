e8d1116194b0cf08233deb54321cbc79428c2797
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-16 21:45:42 +0530

    Compare the length of two lines

--- EqualMethod.java
+++ EqualMethod.java
@@ -2,7 +2,7 @@ package com;
 
 class EqualMethod{
 
-    public boolean EqualMethod(Integer lengthOfLine1, Integer lengthOfLine2) {
+    public boolean equalMethod(Integer lengthOfLine1, Integer lengthOfLine2) {
         System.out.println("Equals Method Result:");
         if(lengthOfLine1.equals(lengthOfLine2))
             return true;
@@ -10,3 +10,4 @@ class EqualMethod{
             return false;
     }
 }
+ggit
